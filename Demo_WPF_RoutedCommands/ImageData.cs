9f4de9e12def08944f3400db768a65552d32381e
John Velis <john@johnvelis.com>
2018-11-12 12:20:47 -0500

    initial push

---
+++ Demo_WPF_RoutedCommands/ImageData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_WPF_MVVMExamples
+{
+    public class ImageData : INotifyPropertyChanged
+    {
+        double _zoom = 1.0;
+
+        public string ImagePath { get; private set; }
+
+        public ImageData(string path)
+        {
+            ImagePath = path;
+        }
+
+        public double Zoom
+        {
+            get { return _zoom; }
+            set
+            {
+                _zoom = value;
+                OnPropertyChanged("Zoom");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string name)
+        {
+            var pc = PropertyChanged;
+            if (pc != null)
+            {
+                pc(this, new PropertyChangedEventArgs(name));
+            }
+
+            //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ Demo_WPF_RoutedCommands/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo_WPF_MVVMExamples
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ImageData _image;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //private void OnOpen(object sender, RoutedEventArgs e)
+        //{
+        //    var dlg = new OpenFileDialog
+        //    {
+        //        Filter = "Image Flies|*.jpg; *.png; *.bmp; *.gif"
+        //    };
+        //    if (dlg.ShowDialog() == true)
+        //    {
+        //        _image = new ImageData(dlg.FileName);
+        //        DataContext = _image;
+        //    }
+        //}
+
+        private void OnIsImageExist(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = _image != null;
+        }
+
+        private void OnZoomIn(object sender, ExecutedRoutedEventArgs e)
+        {
+            _image.Zoom *= 1.2;
+        }
+
+        private void OnZoomOut(object sender, ExecutedRoutedEventArgs e)
+        {
+            _image.Zoom /= 1.2;
+        }
+
+        private void OnOpen(object sender, ExecutedRoutedEventArgs e)
+        {
+            var dlg = new OpenFileDialog
+            {
+                Filter = "Image Flies|*.jpg; *.png; *.bmp; *.gif"
+            };
+            if (dlg.ShowDialog() == true)
+            {
+                _image = new ImageData(dlg.FileName);
+                DataContext = _image;
+            }
+        }
+    }
+}
